3085451b03c8e1dcc20e1d1dfbc46195b8d4c0a8
Manas Ranjan Kar <manasrkar.nmims@gmail.com>
2016-04-25 14:32:53 +0530

    Added files via upload

---
+++ Caravel/twitter_streaming.py
@@ -0,0 +1,39 @@
+'''
+Twitter streaming
+
+Fetches tweets from twitter with given topics
+
+'''
+#Import the necessary methods from tweepy library
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+
+#Variables that contains the user credentials to access Twitter API 
+access_token = "ENTER YOUR ACCESS TOKEN"
+access_token_secret = "ENTER YOUR ACCESS TOKEN SECRET"
+consumer_key = "ENTER YOUR API KEY"
+consumer_secret = "ENTER YOUR API SECRET"
+
+
+#This is a basic listener that just prints received tweets to stdout.
+class StdOutListener(StreamListener):
+
+    def on_data(self, data):
+        print data
+        return True
+
+    def on_error(self, status):
+        print status
+
+
+if __name__ == '__main__':
+
+    #This handles Twitter authetification and the connection to Twitter Streaming API
+    l = StdOutListener()
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    stream = Stream(auth, l)
+
+    #This line filter Twitter Streams to capture data by the keywords: 'IPL2016'
+    stream.filter(track=['IPL2016'])
